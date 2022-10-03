5be0ee63758f3524c1f9aebfc44e002dd9dd42fc
Ursu Nicolae <ursu.nicolae@isa.utm.md>
2022-10-03 16:18:48 +0300

    Remove dublicate code

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
+
+const {ROUTES} = require("./routes");
+
 const {setupLogging} = require("./logging");
+const {setupRateLimit} = require("./ratelimit");
+const {setupProxies} = require("./proxy");
 
 const app = express()
 const port = 3000;
 
 setupLogging(app);
-
-app.get('/hello', (req, resp) => {
-    return resp.send('HELLO WORLD!');
-})
+setupRateLimit(app, ROUTES);
+setupProxies(app, ROUTES);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
