3360c4d39f9c5560f07408a8e7a218292b6ce306
Ursu Nicolae <ursu.nicolae@isa.utm.md>
2022-10-30 21:34:37 +0200

    Finish work on proxy

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const {createProxyMiddleware} = require("http-proxy-middleware")
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+
+
+const PORT =  8000;
+const TARGET =  "http://localhost:5100";
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 1 minute)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "To many request (100 request per 1 minute)",
+});
+
+app.use(limiter);
+
+app.use(express.Router().get('/test', (_, res) => {
+  res.status(200).send('Proxy is OK')
+}))
+
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: TARGET,
+    changeOrigin: true,
+  })
+);
+
+app.listen(PORT, () =>
+  console.log(`Proxy is running on port: ${PORT}`)
+);
